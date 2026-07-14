724f99ec93081364ff3de6792c3a411beb7d44ef
Pasha1234565 <pasha1234565@example.com>
2026-07-14 14:37:26 +0530

    Remove frappe from install_requires to fix uv URL dependency resolution
    Frappe v17 uses pypika as a git URL dependency, and uv doesn't allow URL
    dependencies in transitive editable installs. Frappe apps don't need to
    declare frappe as a pip dependency - bench handles it.

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from __future__ import unicode_literals
 from setuptools import setup, find_packages
 from kirana_ledger import __version__ as version
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
 setup(
     name="kirana_ledger",
     version=version,
@@ -15,5 +12,5 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=install_requires,
+    install_requires=[],
 )
